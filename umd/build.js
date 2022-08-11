cfbfc6bb20315efed09ba8e88dc65866c9012fb1
Michał Malik <m.malik@cksource.com>
2022-08-11 15:13:25 +0200

    Adjust samples

--- umd/build.js
+++ umd/build.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 
-import ejs from 'ejs';
 import fs from 'fs-extra';
 import * as path from 'path';
 import * as url from 'url';
@@ -19,20 +18,7 @@ try {
         fs.copySync(from, to);
     });
 
-    ejs.renderFile(
-        './index.ejs',
-        {
-            ASSETS_ORIGIN: process.env.ASSETS_ORIGIN,
-            API_ORIGIN: process.env.API_ORIGIN,
-            TOKEN_URL: process.env.TOKEN_URL
-        },
-        (err, str) => {
-            if (err) {
-                throw new Error('Could not render EJS');
-            }
-            fs.writeFileSync(path.resolve(dist, 'index.html'), str);
-        }
-    );
+    fs.copySync(path.resolve('index.html'), path.resolve(dist, 'index.html'));
 
     process.exit(0);
 } catch (error) {
